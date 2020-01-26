260a3f502a5f927db23d3ebb818b11ba1bb96f5f
Jacob Stanger <jstangwork@gmail.com>
2020-01-25 19:57:22 -0700

    passing event around now. getting better.

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -20,7 +20,7 @@ let questionsForGame = [
       d: 'with the Durselys'
     },
     answersLength: 4,
-    correctAnswer: 'b'
+    correctAnswer: 'd'
   }
 ];
 
@@ -48,26 +48,30 @@ $(document).ready(function () {
   });
 
   // FIXME: might need to change back to ids
-  $('#a').on('click', function(event) {
-    answerHandler('a');
+  $('.a').on('click', function(event) {
+    // answerHandler('a');
+    answerHandler(event);
   });
 
-  $('#b').on('click', function(event) {
-    answerHandler('b');
+  $('.b').on('click', function(event) {
+    // answerHandler('b');
+    answerHandler(event);
   });
 
-  $('#c').on('click', function(event) {
-    answerHandler('c');
+  $('.c').on('click', function(event) {
+    // answerHandler('c');
+    answerHandler(event);
   });
 
-  $('#d').on('click', function(event) {
-    answerHandler('d');
+  $('.d').on('click', function(event) {
+    // answerHandler('d');
+    answerHandler(event);
   });
 
-  $('.answer').on('click', function() {
-    console.log('answer log');
-    console.log(event);
-  });
+  // $('.answer').on('click', function() {
+  //   console.log('answer log');
+  //   console.log(event);
+  // });
 
 
 
@@ -75,25 +79,36 @@ $(document).ready(function () {
   // HELPER FUNCTIONS
   // ==============================================
 
-  function answerHandler(letter) {
-    var theCorrectAnswer = questionsForGame[whatQuestion].correctAnswer;
-    console.log('you chose: ' + letter + ' correct answer: ' + theCorrectAnswer);
-
-    if ( theCorrectAnswer === letter) {
-      console.log('winner!');
-    } else {
-      console.log('loser!');
-    }
-
+  function answerHandler(event) {
+    // console.log(event);
+    console.log('you chose: ' + event.target.classList[3]);
+    console.log('curr corect answer: ' + questionsForGame[whatQuestion].correctAnswer);
   }
 
+  // function answerHandler(letter) {
+  //   var theCorrectAnswer = questionsForGame[whatQuestion].correctAnswer;
+  //   console.log('you chose: ' + letter + ' correct answer: ' + theCorrectAnswer);
+
+  //   if ( theCorrectAnswer === letter) {
+  //     console.log('winner!');
+  //   } else {
+  //     console.log('loser!');
+  //   }
+
+  // }
+
   function startTriviaGame() {
+    createButton('a');
+    createButton('b');
+    createButton('c');
+    createButton('d');
     loadQuestionAndAnswers();
     // start the timer
   }
 
   function nextQuestion() {
     whatQuestion++;
+    console.log('whatQuestion line 101: ' + whatQuestion);
     clearGameArea();
     loadQuestionAndAnswers();
   }
@@ -104,43 +119,58 @@ $(document).ready(function () {
 
     var currentQuestion = questionsForGame[whatQuestion];
     console.log(currentQuestion);
-    
 
     $('#question-area').text(currentQuestion.question);
 
     var listOfAnswers = currentQuestion.answers;
     if (listOfAnswers['a']) {
-      displayAnswer('a', listOfAnswers['a']);
+      // displayAnswer('a', listOfAnswers['a']);
+      changeText('a', listOfAnswers['a']);
     }
     if (listOfAnswers['b']) {
-      displayAnswer('b', listOfAnswers['b']);
+      // displayAnswer('b', listOfAnswers['b']);
+      changeText('b', listOfAnswers['b']);
     }
     if (listOfAnswers['c']) {
-      displayAnswer('c', listOfAnswers['c']);
+      // displayAnswer('c', listOfAnswers['c']);
+      changeText('c', listOfAnswers['c']);
     }
     if (listOfAnswers['d']) {
-      displayAnswer('d', listOfAnswers['d']);
+      // displayAnswer('d', listOfAnswers['d']);
+      changeText('d', listOfAnswers['d']);
     }
 
     //keeps track of where we are at in the array. 
     // whatQuestion++;
   }
 
-  function displayAnswer(letter, theAnswer) {
-    var newAnswer = $('<p>')
-        .text(letter + '. ' + theAnswer)
-        .attr('id', letter)
-        .addClass('btn btn-secondary answer ' + letter);
-      $('#answer-area').append(newAnswer);
+  function changeText(letter, theAnswer) {
+    $('.' + letter).text();
 
+
+    $('.' + letter).text(letter + '. ' + theAnswer);
   }
+
+  function createButton(letter) {
+    var newAnswer = $('<p>').addClass('btn btn-secondary answer ' + letter);
+    $('#answer-area').append(newAnswer);
+  }
+
+  // function displayAnswer(letter, theAnswer) {
+  //   var newAnswer = $('<p>')
+  //       .text(letter + '. ' + theAnswer)
+  //       .attr('id', letter)
+  //       .addClass('btn btn-secondary answer ' + letter);
+  //     $('#answer-area').append(newAnswer);
+
+  // }
   
   function clearGameArea() {
     // FIXME: 2nd question not clickable
     $('#question-area').empty();
     // $('#answer-area').empty();
     // $('#answer-area').detach();
-    $('.answer').detach();
+    // $('.answer').detach();
   }
 
 
